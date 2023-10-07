cd8f10ce6391109e8b532c627fb3e860b86e1c46
slava <oldfild1@gmail.com>
2023-10-07 04:00:33 +0300

    task 1

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hw_django',
+        'USER': 'postgres',
+        'PASSWORD': 'goldenslav1',
     }
 }
 
